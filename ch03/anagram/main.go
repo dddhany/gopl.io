d84b5b11444c892312eff8fcd0584a14d8cdad05
Dhany <dddhany@gmail.com>
2018-02-19 15:43:39 +0800

    anagram - exercise 3.12

---
+++ ch03/anagram/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func anagram(s string) bool {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		if runes[i] != runes[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Printf("%b\n", anagram("abcdcba"))
+	fmt.Printf("%b\n", anagram("abcdecba"))
+}
